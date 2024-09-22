d7e32fd3868e490793f4dddbaac4712a33946faa
Divyansh Rai <ce.divyanshrai@gmail.com>
2024-09-22 18:27:53 +0530

    final commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,9 +8,12 @@ const Footer = () => {
         </div>
   
         <div className="flex gap-3">
-          <div className="social-icon">
-            <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <div className="social-icon flex items-centerr">
+            
+              <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+            
           </div>
+
           <div className="social-icon">
             <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
           </div>
