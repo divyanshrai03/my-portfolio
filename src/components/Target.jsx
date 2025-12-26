32015c7ec8b4e801fb569ef59d13019f7a4ba830
Divyansh Rai <ce.divyanshrai@gmail.com>
2025-12-26 19:14:31 +0530

    Fix GLTF model loading and animation stability

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,28 +1,37 @@
-import { useGLTF } from '@react-three/drei';
-import { useRef } from 'react';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
+import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Target = (props) => {
-  const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
+  const targetRef = useRef()
+
+  const { scene } = useGLTF('/models/computer.glb')
 
   useGSAP(() => {
+    if (!targetRef.current) return
+
     gsap.to(targetRef.current.position, {
       y: targetRef.current.position.y + 0.5,
       duration: 1.5,
       repeat: -1,
       yoyo: true,
-    });
-  });
+      ease: 'power1.inOut',
+    })
+  })
 
   return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+    <group
+      {...props}
+      ref={targetRef}
+      rotation={[0, Math.PI / 5, 0]}
+      scale={1.5}
+    >
       <primitive object={scene} />
-    </mesh>
-  );
-};
+    </group>
+  )
+}
 
-export default Target;
+useGLTF.preload('/models/computer.glb')
+
+export default Target
